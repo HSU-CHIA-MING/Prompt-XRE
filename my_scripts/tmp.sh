ba1c5a5ad19cc8424c5c521893d91d607ca903b8
HSU-CHIA-MING <95736896+HSU-CHIA-MING@users.noreply.github.com>
2022-09-20 19:37:24 +0800

    Delete my_scripts directory

--- my_scripts/tmp.sh
+++
@@ -1,7 +0,0 @@
-data_home=/workspace/data/datasets/NLG/translation/mBART_data/wmt17-enzh-conllu
-home=/workspace/data/users/zanchangtong1
-SPM=${home}/zanchangtong1/sentencepiece/build/src/spm_encode
-MODEL=${home}/mbart.cc25/sentence.bpe.model
-
-${SPM} --model=${MODEL} < /workspace/data/datasets/NLG/translation/mBART_data/wmt17-enzh-conllu/raw.XIE.zh_CN-en_XX.input0 > /workspace/data/datasets/NLG/translation/mBART_data/wmt17-enzh-conllu/raw.XIE.zh_CN-en_XX.spm.input0 
-
